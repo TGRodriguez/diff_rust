e0c0e04c10c2334c3fc6c651c3509e2990b84830
TGRodriguez <tgrodriguez@fi.uba.ar>
2022-04-26 16:00:49 -0300

    Agrego y mejoro documentación

--- src/grid.rs
+++ src/grid.rs
@@ -1,4 +1,10 @@
-/// Estructura que representa un grilla, similar a una matriz.
+/// Estructura que representa un grilla, similar a una matriz, que almacena enteros sin signar.
+///
+/// # Campos
+///
+/// * `columns` - Usize que representa la cantidad de columnas de la grilla usado para acceder a cualquier posición indexando por filas y columnas.
+/// * `data` - Vector de usize que contiene los valores de la grilla. Los primeros 'columns' elementos serán los elementos de la primera fila.
+/// De esta forma, la posicion (i, j) de la grilla se encuentra en data[(i*columns)+j].
 pub struct Grid {
     columns: usize,
     data: Vec<usize>,
@@ -7,81 +13,83 @@ pub struct Grid {
 impl Grid {
     /// Creador del Grid.
     ///
-    /// Recibe la cantidad de filas y columnas que tendrá el Grid
-    /// y devuelve un Grid con todas las posiciones iniciadas en 0.
-    pub fn new(rows_number: usize, columns_number: usize) -> Grid {
+    /// # Argumentos
+    ///
+    /// * `rows`, `columns` - Usize que indican la cantidad de filas y columnas con la que se creará la grilla.
+    pub fn new(rows: usize, columns: usize) -> Grid {
         Grid {
-            columns: columns_number,
-            data: vec![0; columns_number * rows_number],
+            columns,
+            data: vec![0; columns * rows],
         }
     }
     /// Inserta un número en el Grid en una determinada posición.
     ///
-    /// Recibe los números de fila y columna en las que insertar el
-    /// número que se recibe. Si ya había un elemento, se pisa.
+    /// # Argumentos
+    ///
+    /// * `row`, `column` - Usize que indican la posición (row, column) en la que se insertará el elemento
+    /// * `number` - Usize que representa el elemento a insertar.
     pub fn insert(&mut self, row: usize, column: usize, number: usize) {
         self.data[(row * (self.columns)) + column] = number;
     }
 
-    /// Obtiene el elemento de una determinada posición.
+    /// Obtiene y devuelve el elemento de una determinada posición.
     ///
-    /// Recibe los números de fila y columna de los que se quiere
-    /// obtener el dato, y devuelve ese número.
+    /// # Argumentos
+    ///
+    /// * `row`, `column` - Usize que indican la posición (row, column) de la que se obtendrá el dato.
     pub fn get(&self, row: usize, column: usize) -> usize {
         self.data[(row * (self.columns)) + column]
     }
 }
 
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::*;
 
     #[test]
-    fn grid_creation(){
+    fn grid_creation() {
         let (rows, columns) = (5, 5);
         let grid = Grid::new(rows, columns);
-        for i in 0..rows{
-            for j in 0..columns{
+        for i in 0..rows {
+            for j in 0..columns {
                 assert!(grid.get(i, j) == 0);
             }
         }
-
     }
     #[test]
-    fn grid_insertion(){
+    fn grid_insertion() {
         let (rows, columns) = (5, 5);
         let mut grid = Grid::new(rows, columns);
-        for i in 0..rows{
-            for j in 0..columns{
-                grid.insert(i, j, i+j);
+        for i in 0..rows {
+            for j in 0..columns {
+                grid.insert(i, j, i + j);
             }
         }
-        for i in 0..rows{
-            for j in 0..columns{
-                assert!(grid.get(i,j) == i+j);
+        for i in 0..rows {
+            for j in 0..columns {
+                assert!(grid.get(i, j) == i + j);
             }
         }
     }
 
     #[test]
-    fn grid_replace(){
+    fn grid_replace() {
         let (rows, columns) = (5, 5);
         let mut grid = Grid::new(rows, columns);
-        for i in 0..rows{
-            for j in 0..columns{
-                grid.insert(i, j, i+j);
+        for i in 0..rows {
+            for j in 0..columns {
+                grid.insert(i, j, i + j);
             }
         }
-        for i in 0..rows{
-            for j in 0..columns{
+        for i in 0..rows {
+            for j in 0..columns {
                 grid.insert(i, j, i);
             }
         }
-        for i in 0..rows{
-            for j in 0..columns{
-                assert!(grid.get(i,j) == i);
+        for i in 0..rows {
+            for j in 0..columns {
+                assert!(grid.get(i, j) == i);
             }
         }
-
     }
 }
