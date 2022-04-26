3f94a96bfd5b1f0859172424b1816bafb5b1ee50
TGRodriguez <tgrodriguez@fi.uba.ar>
2022-04-26 14:52:04 -0300

    Corro cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,17 @@ use diff::lcs;
 ///
 /// Se espera que se reciban dos paths a dos archivos
 /// de texto como parámetro.
-fn main() -> Result<(), &'static str> { 
+fn main() -> Result<(), &'static str> {
     let args: Vec<String> = std::env::args().collect();
 
     if args.len() != 3 {
-        return Err("Error: cantidad inválida de argumentos. Uso: diff <un_archivo> <otro_archivo>");
+        return Err(
+            "Error: cantidad inválida de argumentos. Uso: diff <un_archivo> <otro_archivo>",
+        );
     }
-    if let Err(e) = lcs::diff(&args[1], &args[2]){
+    if let Err(e) = lcs::diff(&args[1], &args[2]) {
         println!("Error al generar diff: {}", e);
         return Err("Error en la lectura de archivos");
     }
     Ok(())
 }
-
